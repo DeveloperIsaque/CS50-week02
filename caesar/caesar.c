c05d0cdce57ea8e32227811f4bbc0cd8d0058007
DeveloperIsaque <i27052024@gmail.com>
2024-06-28 00:42:11 -0300

    1.05 - Mexendo no código no 'caesar'.

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,34 +1,42 @@
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
+
+int transforming (int length, string argv[1]);
 
 int main(int argc, string argv[])
 {
     int start = 1;
     int length = strlen(argv[1]);
-    int key[1][length];
 
     for(int i = 0; i < length; i++)
     {
-        if(!(isdigit(argv[1][i])))
+        if(!isdigit(argv[1][i]) || argv[2] != NULL)
         {
             printf("Usage: ./caesar key\n");
             start = 0;
-            break;
-            // validação OK!
-        }
-        else
-        {
-            // Não ta funcionando(está como caractere)
-            key[0][i] = argv[1][i];
+            return 1;
         }
     }
 
     if(start == 1)
     {
-        // Não esta atribuindo o valor
-        int number = key;
+        int number = transforming (length, argv);
     }
     return 0;
+}
+
+int transforming (int length, string array[1])
+{
+    int number = 0;
+    int base = 10;
+    for(int i = 0; i < length; i++)
+    {
+        int exponent = length - i;
+        number = number + (argv[1][i] * (pow(base, exponent)));
+    }
+
+    return number;
 }
